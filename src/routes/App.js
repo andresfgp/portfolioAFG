22893944c6e9510dd4d1697ca6dd1943f249b94d
andresfgp <64049320+andresfgp@users.noreply.github.com>
2021-09-22 18:55:14 -0500

    routePortfolio

--- src/routes/App.js
+++ src/routes/App.js
@@ -9,7 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/portfolioAFG' component={Home} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
